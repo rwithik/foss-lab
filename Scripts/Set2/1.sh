67c0d5d3e87e87245591d79910c1e6fa47700492
Rwithik Manoj <rwithik@gmail.com>
2019-03-21 13:38:05 +0530

    Update 1.sh

--- Scripts/Set2/1.sh
+++ Scripts/Set2/1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -f $1 ]]
+then
+  printf "File does not exist"
+fi
+
 printf "%-15s" "Permission" "Size" "Filename" "Last modified"
 printf "\n"
 ls -l $1 | awk '{printf "%-15s%-15s%-15s%-15s\n", $1, $5, $9, $8}'
