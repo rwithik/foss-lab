e4326dbeedbbbb8014d085ce7269a5c0d374625f
rwithik <rwithik@gmail.com>
2019-03-28 15:27:08 +0530

    minor edits

--- Scripts/Set2/5.sh
+++ Scripts/Set2/5.sh
@@ -6,7 +6,8 @@ then
 	exit
 fi
 
-echo "Vowels : `cat $1 | tr '[a-z]' '[A-Z]' | grep -o "A\|E\|I\|O\|U" | wc -l`"
+echo "Vowels : `cat $1 | tr '[a-z]' '[A-Z]' | egrep -o "[AEIOU]" | wc -l`"
+echo "Symbols : `cat $1 | tr '[a-z]' '[A-Z]' | egrep -o "[.!''"",()]" | wc -l`"
 echo "Characters : $(cat $1 | wc -c)"
 echo "Blank lines : $(grep  -c '^$' $1)"
 echo "Lines : $(cat $1 | wc -l )"
